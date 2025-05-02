d056a9f6c4a34b47d99dd324c23b42793ac37d30
André Santos <andremissional@gmail.com>
2025-05-02 16:47:23 -0300

    explorando-console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,17 @@
+//impressão de mais de uma valor
+const x = 10
+const y ='André'
+const z = [1,2]
+
+console.log(x,y,z)
+
+//contagem de impressão
+
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+// variaveis entre strings
+
+console.log("O nome dele é %s, e ele é programador", y)
