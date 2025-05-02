c7d7d0a5ee424d03bf37250f67b7c006a99f9084
André Santos <andremissional@gmail.com>
2025-05-02 19:37:05 -0300

    Update index.js

--- 2_FUNDAMENTOS/8_chalk/index.js
+++ 2_FUNDAMENTOS/8_chalk/index.js
@@ -1,5 +1,9 @@
 const chalk = require("chalk")
 
-const nota = 9
+const nota = 5
 
-console.log(chalk.green('Parabéns! Você está aprovado'))
+if (nota >=7) {
+    console.log(chalk.green.bold('Parabéns! Você está aprovado'))
+} else {
+    console.log(chalk.bgRed('Você está de RECUPERAÇÃO'))
+}
