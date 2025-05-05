f22469d9a5bbb3008224292e3c815417a6598092
André Santos <andremissional@gmail.com>
2025-05-05 12:24:23 -0300

    Update index.js

--- 2_FUNDAMENTOS/5_modulos_externos/index.js
+++ 2_FUNDAMENTOS/5_modulos_externos/index.js
@@ -1,13 +1,14 @@
 const minimist = require("minimist")
 
-const args =minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2))
 
 console.log(args)
 
 const nome = args['nome']
 const profissao = args['profissao']
+const time = args ['time']
 
-console.log(nome,profissao)
+console.log(nome,profissao,time)
 
-console.log(`O nome dele é ${nome} e a profissão é ${profissao}`)
+console.log(`O nome dele é ${nome} e a profissão é ${profissao} e o time é ${time}`)
 
