0f9541b5b9f9190fe0b80dd4d366d60cdb1a626d
André Santos <andremissional@gmail.com>
2025-05-06 18:35:27 -0300

    Update index_professor.js

--- 2_FUNDAMENTOS/15_tarefa_02/index_professor.js
+++ 2_FUNDAMENTOS/15_tarefa_02/index_professor.js
@@ -1,6 +1,14 @@
 const inquirer = require("inquirer")
-
-const inquire =('inquire')
-const chalk = ('chalk')
+const chalk = require('chalk')
 
 inquirer
+    .prompt([
+        { name: 'nome', message: 'Qual o seu nome?' },
+        { name: 'idade', message: 'Qual a sua idade?' },
+    ])
+    .then((answers) => {
+        const response = `O nome do usúario é: ${answers.nome}, e a idade é: ${answers.idade}`
+
+        console.log(chalk.bgYellosw.black(response))
+    })
+    .catch((err) => console.logg(err))
