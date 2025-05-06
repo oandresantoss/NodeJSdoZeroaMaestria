9ec22407681e4c29ac96ddb1480472644cb63025
André Santos <andremissional@gmail.com>
2025-05-06 18:47:27 -0300

    Update index_professor.js

--- 2_FUNDAMENTOS/15_tarefa_02/index_professor.js
+++ 2_FUNDAMENTOS/15_tarefa_02/index_professor.js
@@ -7,8 +7,12 @@ inquirer
         { name: 'idade', message: 'Qual a sua idade?' },
     ])
     .then((answers) => {
+        if (!answers.nome || !answers.idade) {
+            throw new Error('O nome e a idade são obrigatórios!')
+        }
+
         const response = `O nome do usuário é: ${answers.nome} e ele tem: ${answers.idade} anos`
 
-        console.log(chalk.bgYellosw.black(response))
+        console.log(chalk.bgYellow.black(response))
     })
     .catch((err) => console.log(err))
