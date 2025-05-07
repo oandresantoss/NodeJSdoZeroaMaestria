452122f2c21494c397808c5f92e050ba9020a43c
André Santos <andremissional@gmail.com>
2025-05-07 20:06:53 -0300

    Update index.js

--- 2_FUNDAMENTOS/7_explorando_console/index.js
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -6,16 +6,16 @@ const z = [1,2]
 console.log(x,y,z)
 
 //contagem de impressão
-
 console.count(`O valor de x é: ${x}, contagem`)
 console.count(`O valor de x é: ${x}, contagem`)
 console.count(`O valor de x é: ${x}, contagem`)
 console.count(`O valor de x é: ${x}, contagem`)
 
 // variaveis entre strings
-
 console.log("O nome dele é %s, e ele é programador", y)
 
+
+// Limpeza do console
 setTimeout(() => {
     console.clear()
 }, 2000)
