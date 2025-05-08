2ecc27fe3e291a344856a4408579cb5259990499
André Santos <andremissional@gmail.com>
2025-05-07 22:50:38 -0300

    revisão

--- 2_FUNDAMENTOS/10_abstracao_input/index.js
+++ 2_FUNDAMENTOS/10_abstracao_input/index.js
@@ -1,18 +1,12 @@
 const inquirer = require('inquirer')
 
 inquirer.prompt([
-    {
-        name: 'p1',
-        message: 'Qual a primeira nota?',
-    },
-    {   
-        name: 'p2',
-        message: 'Qual a segunda nota?',
-
-    },
+    {name: 'p1',message: 'Qual a primeira nota?',},
+    {name: 'p2',message: 'Qual a segunda nota?',},
+    
 ]).then((answers) => {
     console.log(answers)
-    const media = (parseInt(answers.p1) + parseInt(answers.p2)) / 2
+    const media = (Number(answers.p1) + Number(answers.p2)) / 2
 
     console.log(`A média é: ${media}`)
 })
--- 2_FUNDAMENTOS/14_erros/throw.js
+++ 2_FUNDAMENTOS/14_erros/throw.js
@@ -1,7 +1,7 @@
 const x = 10
 //const x = '10'
 
-// checar se é número
+// checar se é número inteiro
 if(!Number.isInteger(x)) {
     throw new Error('O valor de x não é um número inteiro')
 }
