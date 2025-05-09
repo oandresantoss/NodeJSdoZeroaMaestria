aaaef994aa587145707a70592ad245e36e2d976a
André Santos <andremissional@gmail.com>
2025-05-09 13:05:27 -0300

    Update index.js

--- 3_CORE_MODULES/index.js
+++ 3_CORE_MODULES/index.js
@@ -1,14 +1,14 @@
-
 const http = require("http")
 
 const port = 3000
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200
-    res.setHeader('contenty-typr', 'text/html')
-    res.end('<h1>Olá esse é meu primeiro sever com html!</h1>')
+const server = http.createServer((req, res) => {
+
+    res.write('Oi HTTP')
+    res.end()
 })
 
 server.listen(port, () => {
-    console.log(`servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`)
 })
+
