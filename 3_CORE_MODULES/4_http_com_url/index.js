b1042b1d8fe95adf825ef31ab3f509d013124da4
André Santos <andremissional@gmail.com>
2025-05-09 16:42:01 -0300

    http+url

---
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -0,0 +1,16 @@
+
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req,res) => {
+
+
+    res.statusCode = 200
+    res.setHeader('contenty-typr', 'text/html')
+    res.end('<h1>Olá esse é meu primeiro sever com html!</h1><p>Testando atualização</p>',)
+})
+
+server.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`)
+})
