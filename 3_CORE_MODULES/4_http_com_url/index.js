0c1209432a7975d7ce013db22af6ba98c96355d0
André Santos <andremissional@gmail.com>
2025-05-09 17:17:13 -0300

    Update index.js

--- 3_CORE_MODULES/4_http_com_url/index.js
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -6,15 +6,15 @@ const port = 3000
 const server = http.createServer((req,res) => {
 
     const urlInfo = require('url').parse(req.url, true)
-    const nome = urlInfo.query.nome
+    const name = urlInfo.query.name
 
     res.statusCode = 200
-    res.setHeader('contenty-typr', 'text/html')
+    res.setHeader('content-type', 'text/html')
     
-    if(!nome) {
-        ReadableStreamDefaultController('<h1>Preencha o seu nome:</h1><form method="GET"><imput type="text" nome="nome"/><input type="submit" value="Enviar"></form>')
-    }else {
-        res.end(`<h1>Seja bem vindo ${nome}!</h1>`)
+    if(!name) {
+        res.end('<h1>Preencha o seu nome:</h1><form method="GET"><input type="text" name="name"/><input type="submit" value="Enviar"></form>')
+    } else {
+        res.end(`<h1>Seja bem vindo ${name}!</h1>`)
     }
 })
 
